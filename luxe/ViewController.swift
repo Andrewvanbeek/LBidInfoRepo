3ab85ff005a89b3ac4c8a222fe05c47dc4a7fca8
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-05-25 19:52:31 -0500

    Initial Commit

---
+++ luxe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  luxe
+//
+//  Created by Matthew Ramuta on 5/25/17.
+//  Copyright © 2017 Luxe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
