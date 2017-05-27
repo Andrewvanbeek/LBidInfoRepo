5e11cd27b74fc97f20fbe34403fb4ed22d9d8caa
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-05-27 18:02:35 -0500

    Add nav controller create home and launch screen

--- luxe/HomeScreen.swift
+++ luxe/HomeScreen.swift
@@ -1,29 +1,28 @@
-
-//  ViewController.swift
+//
+//  HomeScreen.swift
 //  luxe
 //
-//  Created by Matthew Ramuta on 5/25/17.
+//  Created by Matthew Ramuta on 5/27/17.
 //  Copyright © 2017 Luxe. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeScreen: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.navigationController?.isNavigationBarHidden = true 
+        self.navigationController?.isNavigationBarHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-//    self.navigationController.navigationBar.hidden = YES;
-//    @IBOutlet var homeScreen: UIView!
-//    homeScreen.navigationController.navigationBar.hidden = YES;
-
+    
+    //    self.navigationController.navigationBar.hidden = YES;
+    //    @IBOutlet var homeScreen: UIView!
+    //    homeScreen.navigationController.navigationBar.hidden = YES;
+    
 }
-
