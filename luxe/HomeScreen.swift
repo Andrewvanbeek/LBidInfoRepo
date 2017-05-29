396e1081ea2c7b628ef221b4e08b266c58629d90
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-05-28 21:20:43 -0500

    replace some segues with unwinds

--- luxe/HomeScreen.swift
+++ luxe/HomeScreen.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class HomeScreen: UIViewController {
     
+    @IBAction func unwindToRegisterScreen(segue: UIStoryboardSegue) {
+        //nothing goes here
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.tintColor = UIColor.white
 //        self.navigationController?.isNavigationBarHidden = false
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
